89d2b0c62164e351885ad43a2720f269c4351497
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2022-01-21 12:46:32 +0100

    Ajout feature rechercher un film

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,19 @@ import Card from './Card';
 
 const Form = () => {
     const [moviesData, setMoviesData] = useState([]);
+    const [search, setSearch] = useState("star")
     
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=star&language=fr-FR`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${search}&language=fr-FR`)
         .then((res) => setMoviesData(res.data.results))
-    }, [])
+    }, [search])
     
     
     return (
         <div className="form-component">
             <div className="form-container">
                 <form>
-                    <input type="text" placeholder="Entrez le titre d'un film" id='search-input' />
+                    <input type="text" placeholder="Entrez le titre d'un film" id='search-input' onChange={(e) => setSearch(e.target.value)} />
                     <input type="submit" value="Rechercher" />
                 </form>
 
